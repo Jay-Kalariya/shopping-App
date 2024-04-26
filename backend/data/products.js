ac4962ec895ed3bdcb736dfc997205f928458f9d
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-04-26 13:32:01 +0530

    rer

---
+++ backend/data/products.js
@@ -0,0 +1,112 @@
+const products = [
+  {
+    name: "Murali Krishna Sweets (Nellore)",
+    imageUrl:
+      "https://www.jiosweet.com/images/thumbs/0003773_badusha_550.jpeg",
+    description:
+      "A Traditional Sweet originating from South India.Sweet made with a mixture of Maida and Sugar powder.Looks like a doughnut immersed in sugar syrup.Prepared freshely with no added flavors and preservatives.Shell Life : 20Days.",
+    price: 499,
+    countInStock: 15,
+  },
+  {
+    name: "Motichoor Laddu",
+    imageUrl:
+      "https://www.sendbestgift.com/assets/images/product/4d7c1935dd31ea038bbd31e7e801c7f4.webp",
+    description:
+      "Craving for sweets is never-ending. Festivals are lined up and people are going crazy which sweet to distribute to their dear ones. Yes, it is motichoor laddu. One cannot resist these laddus at any cost during festivities like Diwali, Janmashtmi, and Ganesha Chathurthi.Nothing can satiate the taste buds apart from these laddus. These laddus are soft and delicious and can be ordered online. Now, ordering motichoor laddu online is just few simple steps.Do you know how to make motichoor laddu at home? It is made with gram flour, spices, and sugar. Tiny balls of laddus are made and mixed with sugar syrup.",
+    price: 1099,
+    countInStock: 10,
+  },
+  {
+    name: "Kaju Burfi",
+    imageUrl:
+      "https://www.sendbestgift.com/assets/images/product/77f83d97b6478d8f3a3f011672a3f7a6.webp",
+    description:
+      "Everyone knows the fact that weddings and festivals are incomplete without kaju barfi. Basically, kaju barfi sweet, is the most famous and traditional Indian sweet. It is a cashew fudge candy and is really smooth and immediately melts in mouth. This lovely sweet kaju barfi can also be customized as per your favorite flavor. It is prepared with extra rick quality of ingredients and this is why cost of kaju barfi is higher. Though, kaju barfi price per kg is a bit on higher side but it is actually the best treat for special occasions. This dessert will definitely quench the taste buds of your loved ones. Have a look at the kaju barfi images and compare kaju barfi price and then place your order.Do not wait anymore, and opt for kaju burfi sweets delivery near me within few hours from the comfort of your home.",
+    price: 1300,
+    countInStock: 5,
+  },
+  {
+    name: "Mix Sweets",
+    imageUrl:
+      "https://www.sendbestgift.com/assets/images/product/b427970b6afadd10dfe50a5e833d5d72.webp",
+    description:
+      "These Sweets Are Made With Premium Ingredients Such As Pure Ghee, Saffron, And High-Quality Nuts, And Are Often Decorated With Edible Silver Foil Or Gold Leaf. Premium Mix Sweets Are Usually Given As Gifts For Weddings, Anniversaries, And Other Special Events And Are Considered A Luxurious Treat.",
+    price: 1550,
+    countInStock: 25,
+  },
+  {
+    name: "Soan Papri",
+    imageUrl:
+      "https://www.sendbestgift.com/assets/images/product/201810261540537003.webp",
+    description:
+      "Soan papdi (also known as san papri, shompapri, sohan papdi, shonpapdi or patisa) is a popular dessert in the Indian subcontinent. It is made of gram flour (besan), all-purpose flour, ghee, sugar and milk. It is usually cube-shaped or served as flakes, and has a crisp and flaky texture.",
+    price: 233,
+    countInStock: 4,
+  },
+  {
+    name: "Besan Burfi",
+    imageUrl:
+      "https://www.sendbestgift.com/assets/images/product/f8997953714d2beefabf47c37f1dc879.webp",
+    description:
+      "It is made of purest form of Gram Flour. It is very delicious and nutritious and has a very long shelf life. Ingredients: Gram pulse flour, sugar, almond, pistachios, silver leaves. Shelf Life : 7 Days",
+    price: 140,
+    countInStock: 10,
+  },
+  {
+    name: "Kaju Pista Roll",
+    imageUrl:
+      "https://www.sendbestgift.com/assets/images/product/5925d3f2f74f583ef783dcba71e40d8f.webp",
+    description:
+      "Kaju Pista Roll is one of Sarvesh’s favorite mithai. Well the fact is that he has a massive sweet tooth and has a long list of favorites so I really don’t know which one is his most favorite but this is right up there.",
+    price: 2204,
+    countInStock: 4,
+  },
+  {
+    name: "Rasgulla",
+    imageUrl:
+      "https://www.sendbestgift.com/assets/images/product/438c94ac55752d4dbe6c16ca5fa7045e.webp",
+    description:
+      "Rasgulla is a syrupy dessert popular in the eastern part of South Asia. It is made from ball-shaped dumplings of chhena dough, cooked in light sugar syrup. This is done until the syrup permeates the dumplings.",
+    price: 180,
+    countInStock: 10,
+  },
+  {
+    name: "Coconut Burfi",
+    imageUrl:
+      "https://www.orderyourchoice.com/114679-large_default/chennai-sweets-tirunelveli.jpg",
+    description:
+      "Coconut Burfi is a Rich, Delicious, Hyginically prepared and is a perfect choice to your dears to make them Happier on Birthdays, Festivals and on all Occasion.",
+    price: 220,
+    countInStock: 10,
+  },
+  {
+    name: "Milk Cake",
+    imageUrl:
+      "https://www.orderyourchoice.com/114975-large_default/chennai-sweets-to-tirunelveli.jpg",
+    description:
+      "Milk Cake - 500gm  is a Rich, Delicious, Hyginically prepared  and is a perfect choice to your dears to make them Happier on Birthdays, Festivals and on all Occasion.",
+    price: 630,
+    countInStock: 10,
+  },
+  {
+    name: "Mysorepauk",
+    imageUrl:
+      "https://www.orderyourchoice.com/9712-large_default/send-sweets-chennai-sweets.jpg",
+    description:
+      "Mysore pak is an Indian sweet prepared in ghee. It originated in the city of Mysore, one of the major cities in the Indian state of Karnataka.",
+    price: 748,
+    countInStock: 4,
+  },
+  {
+    name: "Gulab Jamun",
+    imageUrl:
+      "https://www.orderyourchoice.com/114677-large_default/buy-chennai-sweets-online-nellia.jpg",
+    description:
+      "Gulab Jamum is a Rich, Delicious, Hyginically prepared and is a perfect choice to your dears to make them Happier on Birthdays, Festivals and on all Occasion.",
+    price: 748,
+    countInStock: 10,
+  },
+];
+
+module.exports = products;
