9e2536ef9988f7f9961ddd50cd7dbd7e7dfa5df3
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-05-08 16:13:10 +0530

    sas

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://user-123:user123@cluster0.7ygctfw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
